1ace860fc0b9240254aeea1f714ae07f81fb4fad
Unish Khadka <unishk152@gmail.com>
2024-10-08 15:30:52 -0500

    initialized repo

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import Navigation from "./components/Navigation";
+export const metadata = {title: "The Wild Oasis"}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Navigation from "./components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello Next</h1>
+    </div>
+  );
+}
