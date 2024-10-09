7fd1fc285ee2d8c0eec6aad7fee52e0e62df2a3f
Unish Khadka <unishk152@gmail.com>
2024-10-09 10:27:21 -0500

    updatd code and added other components

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
+import Counter from "./components/Counter";
+import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
-export const metadata = {title: "The Wild Oasis"}
+
+export const metadata = { title: "The Wild Oasis" };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Logo />
         <Navigation />
         <main>
           {children}
         </main>
+        <Counter/>
       </body>
     </html>
   );
